3384f34a94af0af66028a4d97552f8890479c745
sb <sb>
2019-10-19 16:29:47 +0200

    small logo + started nav

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,13 +1,17 @@
 <template>
-    <h1 class = "logo">
+    <h1 v-if = "size === 'big'" class = "logo">
       $DAI daddy
     </h1>
+    <h2 v-else class = "smalllogo">
+        $DAI daddy
+    </h2>
 </template>
 
 <script>
 console.log(this);
 export default {
-  name: 'Logo'
+  name: 'Logo',
+  size: String
 }
 </script>
 
@@ -27,6 +31,49 @@ export default {
         height: 148.51px;
         font-size: 80px;
         line-height: 92px;
+        display: flex;
+        align-items: center;
+    
+	&:before {
+		left:-7px;
+		border-left:20px solid #FFF8F9;
+		border-bottom-right-radius: 16px 14px;
+		transform:translate(0, -2px);
+        content:"";
+		position:absolute;
+        bottom:-2px;
+        height:20px;
+	}
+
+	&:after {
+		left:4px;
+		width:26px;
+		background:white;
+		border-bottom-right-radius: 10px;
+		transform:translate(-30px, -2px);
+        content:"";
+		position:absolute;
+        bottom:-2px;
+        height:20px;
+    }
+}
+    .smalllogo{
+        word-wrap: break-word;
+        margin-bottom: 12px;
+        line-height: 24px;
+        position:relative;
+	    padding:10px 20px;
+        border-radius:25px;
+        font-family: 'Cooper Black';
+        background:#FFF8F9;
+	    color:#FF94CE;
+        position: absolute;
+        width: 266px;
+        height: 61.25px;
+        position: absolute;
+        font-size: 36px;
+        line-height: 41px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         display: flex;
         align-items: center;
     
--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import './registerServiceWorker'
 import Button from 'ant-design-vue/lib/button';
 import MessageBubble from './components/MessageBubble';
 import Logo from './components/Logo';
+import Nav from './components/Nav';
 import 'ant-design-vue/dist/antd.css';
 
 Vue.component(Button.name, Button);
 Vue.component(MessageBubble.name, MessageBubble);
-Vue.component(Logo.name, Logo)
+Vue.component(Logo.name, Logo);
+Vue.component(Nav.name, Nav);
 
 Vue.config.productionTip = false
 
