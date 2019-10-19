85ae274f678bb0483660d60b5553832b59ec67f6
Christopher Maree <christopher.maree@gmail.com>
2019-10-19 12:55:16 +0200

    Set up web3 vue interfaces

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,79 @@
   </div>
 </template>
 
+
+<script>
+/* global web3:true */
+
+import Web3 from "web3";
+import * as actions from "@/store/actions";
+import * as mutations from "@/store/mutation-types";
+import ClickableAddress from "@/components/widgets/ClickableAddress";
+import { mapActions, mapState } from "vuex";
+import router from "@/router";
+
+export default {
+  name: "app",
+  components: { ClickableAddress },
+  data() {
+    return {
+      web3Detected: true,
+      menuVisible: false
+    };
+  },
+  methods: {
+    ...mapActions(["INIT_APP"]),
+    redirect(_path) {
+      router.push({ name: _path });
+    }
+  },
+  async mounted() {
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum);
+      console.log("web3 provider detected!");
+      console.log(window.web3);
+
+      // Request account access if needed
+      ethereum
+        .enable()
+        .then(value => {
+          console.log("Bootstrapping web app - provider acknowedgled", value);
+          this.INIT_APP(window.web3);
+        })
+        .catch(error => {
+          console.log(
+            "User denied access, boostrapping application using infura",
+            error
+          );
+          window.web3 = new Web3(
+            new Web3.providers.HttpProvider(
+              "https://mainnet.infura.io/v3/fb32a606c5c646c7932e43cfaf6c39df"
+            )
+          );
+          this.INIT_APP(window.web3);
+        });
+    } else if (window.web3) {
+      console.log("Running legacy web3 provider");
+      window.web3 = new Web3(web3.currentProvider);
+      this.INIT_APP(window.web3);
+    } else {
+      window.web3 = new Web3(
+        new Web3.providers.HttpProvider(
+          "https://mainnet.infura.io/v3/fb32a606c5c646c7932e43cfaf6c39df"
+        )
+      );
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+      this.INIT_APP(window.web3);
+    }
+  },
+  computed: {
+    ...mapState(["currentNetwork", "account"])
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,95 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Web3 from "web3";
+import Vuex from "vuex";
+import Vue from "vue";
+import axios from "axios";
+import createPersistedState from "vuex-persistedstate";
+import moment from "moment";
+
+import {
+  getEtherscanAddress,
+  getNetIdString,
+}
+from "@/utils/lookupTools";
+
+import * as actions from "./actions";
+import * as mutations from "./mutation-types";
+
+import truffleContract from "truffle-contract";
+
+// import FundFactoryABI from "../../build/contracts/FundFactory.json"
+// const FundFactory = truffleContract(FundFactoryABI);
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    web3: null,
+    account: null,
+    currentNetwork: null,
+    etherscanBase: null,
+    TokenInfo: null,
   },
   mutations: {
+    //WEB3 Stuff
+    [mutations.SET_ACCOUNT](state, account) {
+      state.account = account;
+    },
+    [mutations.SET_CURRENT_NETWORK](state, currentNetwork) {
+      state.currentNetwork = currentNetwork;
+    },
+    [mutations.SET_ETHERSCAN_NETWORK](state, etherscanBase) {
+      state.etherscanBase = etherscanBase;
+    },
+    [mutations.SET_WEB3]: async function (state, web3) {
+      state.web3 = web3;
+    },
   },
   actions: {
-  },
-  modules: {
+    [actions.GET_CURRENT_NETWORK]: function ({
+      commit,
+      dispatch,
+      state
+    }) {
+      getNetIdString().then(currentNetwork => {
+        commit(mutations.SET_CURRENT_NETWORK, currentNetwork);
+      });
+      getEtherscanAddress().then(etherscanBase => {
+        commit(mutations.SET_ETHERSCAN_NETWORK, etherscanBase);
+      });
+    },
+
+    [actions.INIT_APP]: async function ({
+      commit,
+      dispatch,
+      state
+    }, web3) {
+      // FundFactory.setProvider(web3.currentProvider)
+      // Fund.setProvider(web3.currentProvider)
+      // Set the web3 instance
+      console.log("IN STORE")
+      console.log(web3)
+      commit(mutations.SET_WEB3, {
+        web3
+      });
+      console.log("set")
+
+      dispatch(actions.GET_CURRENT_NETWORK);
+
+      let accounts = await web3.eth.getAccounts();
+      let account = accounts[0];
+      if (account) {
+        commit(mutations.SET_ACCOUNT, account);
+      }
+      // let fundFactory = await FundFactory.at(state.factoryAddress)
+      // console.log("logging vyper from UI")
+      // let numberOfFunds = await fundFactory.getAllFundUids()
+      // state.numberOfFunds = numberOfFunds.toString(10)
+      // console.log(numberOfFunds.toString())
+      // commit(mutations.SET_NUMBER_OF_FUNDS, numberOfFunds.toString());
+
+      // let userFunds = await fundFactory.getFundForOwner.call(state.account)
+      // console.log("USER FUNDS")
+      // console.log(userFunds)
+    }
   }
 })
