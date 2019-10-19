3af3ae78e07a0c675ac3636afe9a82501f2ba82c
Christopher Maree <christopher.maree@gmail.com>
2019-10-19 22:57:14 +0200

    Added sell debt page

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from "vue";
-import { Button, Row, Col } from "ant-design-vue";
+import {
+  Button,
+  Row,
+  Col,
+  Modal,
+  Radio
+} from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 
 import App from "./App.vue";
@@ -21,6 +27,8 @@ Vue.component(Logo.name, Logo);
 Vue.component(Nav.name, Nav);
 Vue.component(Messages.name, Messages);
 Vue.component(Hero.name, Hero);
+Vue.component(Modal.name, Modal)
+Vue.component(Radio.name, Radio)
 
 Vue.config.productionTip = false;
 
