300c8ea78c08d68a217eb40715a1989439bee100
Hamed Ali <123hamed@gmail.com>
2019-10-19 19:10:11 +0200

    fix log o styling

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -13,29 +13,25 @@ export default {
 .large {
   word-wrap: break-word;
   margin-bottom: 12px;
-  line-height: 24px;
   padding: 10px 20px;
   border-radius: 25px;
-  font-family: "Cooper Black";
+  font-family: cooper-black-std, serif;
   background: #fff8f9;
   color: #ff94ce;
   height: 148.51px;
   font-size: 80px;
-  line-height: 92px;
   max-width: 594px;
 }
 .small {
   word-wrap: break-word;
   margin-bottom: 12px;
-  line-height: 24px;
   padding: 10px 20px;
   border-radius: 25px;
-  font-family: "Cooper Black";
+  font-family: cooper-black-std, serif;
   background: #fff8f9;
   color: #ff94ce;
   height: 61.25px;
   font-size: 36px;
-  line-height: 41px;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 }
 </style>
