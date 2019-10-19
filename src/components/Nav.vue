ba63d38f34d9e096e5b99f54f184dec38fa6a5c2
Christopher Maree <christopher.maree@gmail.com>
2019-10-19 22:57:31 +0200

    Added new menue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,29 +1,26 @@
 <template>
-    <a-row type="flex" align="center">
-        <a-col :span="12">
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <Logo size="small" />
-                </li>
-                <li class="nav-item">
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/market">Market</router-link>
-                </li>
-            </ul>
-        </a-col>
-        <a-col :span="12">
-            <ul class="nav-list">
-                <li class="nav-item">
-                    <router-link to="/listing">Listing</router-link>
-                </li>
-                <!-- <li class="nav-item">
-                    metamask placeholder spot
-                </li> -->
-            </ul>
-        </a-col>
-    </a-row>
+  <a-row type="flex" style="text-align:center">
+    <a-col :span="12">
+      <ul class="nav-list">
+        <li class="nav-item">
+          <Logo size="small" />
+        </li>
+        <li class="nav-item">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/buy">Market</router-link>
+        </li>
+      </ul>
+    </a-col>
+    <a-col :span="12" type="flex" style="text-align:center">
+      <ul class="nav-list">
+        <li class="nav-item">
+          <router-link to="/sell">My Listing</router-link>
+        </li>
+      </ul>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
@@ -33,27 +30,27 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    .nav{
-        padding: 0px 80px;
-        box-sizing: border-box;
-        display: flex;
-        align-items: center;
-        font-size: .9rem;
-    }
+.nav {
+  padding: 0px 80px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  font-size: 0.9rem;
+}
 
-    .nav-list {
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
+.nav-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
-    .nav-item {
-        display: inline-block;
-        margin-right: 2.25rem;
-    }
+.nav-item {
+  display: inline-block;
+  margin-right: 2.25rem;
+}
 
-    .nav-item {
-        display: inline-block;
-        margin-right: 2.25rem;
-    }
+.nav-item {
+  display: inline-block;
+  margin-right: 2.25rem;
+}
 </style>
