98918cdb801d9dd3835e75180710d95dd0da0262
Hamed Ali <123hamed@gmail.com>
2019-10-19 16:33:26 +0200

    Add messages

--- src/main.js
+++ src/main.js
@@ -1,23 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
-import Button from 'ant-design-vue/lib/button';
-import MessageBubble from './components/MessageBubble';
-import Logo from './components/Logo';
-import Nav from './components/Nav';
-import 'ant-design-vue/dist/antd.css';
+import Vue from "vue";
+import { Button, Row, Col } from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import MessageBubble from "./components/MessageBubble";
+import Messages from "./components/Messages";
+import Logo from "./components/Logo";
+import Nav from "./components/Nav";
+
+Vue.component(Button.name, Button);
+Vue.component(Row.name, Row);
+Vue.component(Col.name, Col);
 Vue.component(Button.name, Button);
 Vue.component(MessageBubble.name, MessageBubble);
 Vue.component(Logo.name, Logo);
 Vue.component(Nav.name, Nav);
+Vue.component(Messages.name, Messages);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
