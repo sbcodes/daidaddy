426da57581df6a216672e90eb5689dcc6b70657a
sbcodes <stefan.bachmann123@gmail.com>
2019-10-19 16:29:47 +0200

    small logo + started nav

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
+  
   <div class="hello">
+    <Nav></Nav>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -33,7 +35,7 @@
       <MessageBubble msgType="from" msg="yes very xd"></MessageBubble>
     </div>
     <div style="align: center">
-      <Logo></Logo>
+      <Logo size="small"></Logo>
     </div>
   </div>
 </template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="nav">
+        <Logo size="small"></Logo>
+    </div>
+</template>
+
+<script>
+console.log(this);
+export default {
+  name: 'Nav'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .nav{
+        padding: 38px 76px;
+    }
+</style>
